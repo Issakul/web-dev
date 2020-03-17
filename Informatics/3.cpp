c9ef56f0a688c26e86782a19806dcb645f9dc080
Kazyna <45530319+Issakul@users.noreply.github.com>
2020-03-17 15:16:38 +0600

    Add files via upload

---
+++ Informatics/3.cpp
@@ -0,0 +1,14 @@
+ 
+ #include <bits/stdc++.h>
+
+using namespace std;
+ 
+ 
+ int n1,n2;
+  cin>>n1>>n2;
+  if ((n1!=1 && n2!=1) || (n1==1 && n2==1))
+    cout<<"YES";
+  else
+    cout<<"NO";
+    return 0;
+    }
---
+++ Informatics/4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+  cin>>n;
+  if (n>0)cout<<1;
+  if (n<0)cout<<-1;
+  if (n==0)cout<<0;
+
+  return 0; 
+  } 
+
